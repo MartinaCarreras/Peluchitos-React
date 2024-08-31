ea6d7d3c94019a5777b6bffb4282bea066a8124e
Martina <martina.carreras3@outlook.com>
2024-08-31 18:03:59 -0300

    1.0.2

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,41 +1,33 @@
 import { Link } from "react-router-dom";
-import style from "./Header.module.css";
-const { header, logo, header_1, menu, menuBox, hiddenMenu } = style;
-import { IoIosMenu } from "react-icons/io";
+import { HiddenMenu } from "./HiddenMenu.jsx";
+import './header.css';
 
 export const Header = () => {
   return (
     <header>
       <nav>
-        <ul className={header}>
+        <ul className="header">
           <Link to="/">
             <img
               src="https://i.imgur.com/DjsgGIx.jpg"
               alt="logo"
-              className={logo}
+              className="logo"
             />
           </Link>
           <Link to="/productos">
-            <li className={header_1}>Productos</li>
+            <li className="header_li_1">Productos</li>
           </Link>
           <Link to="/productos">
-            <li className={header_1}>Cotización</li>
+            <li className="header_li_1">Cotización</li>
           </Link>
           <Link to="/productos">
-            <li className={header_1}>Blog</li>
+            <li className="header_li_1">Blog</li>
           </Link>
           <Link to="/productos">
-            <li className={header_1}>Contacto</li>
+            <li className="header_li_1">Contacto</li>
           </Link>
-          <label htmlFor="menuHamb"><IoIosMenu className={menu}/></label>
-          <input type="checkbox" className={menuBox} id="menuHamb"/>
-          <ul className={ hiddenMenu }>
-            <li>Productos</li> 
-            <li>Cotización</li> 
-            <li>Blog</li> 
-            <li>Contacto</li> 
-          </ul>
         </ul>
+        <HiddenMenu/>
       </nav>
     </header>
   );
--- src/components/layout/header/HiddenMenu.jsx
+++ src/components/layout/header/HiddenMenu.jsx
@@ -1,13 +1,31 @@
-import style from './Header.module.css';
-const { hiddenMenu } = style;
+import './header.css';
+import { SlArrowDown } from "react-icons/sl";
+import { Link } from 'react-router-dom';
 
 export const HiddenMenu = () => {
   return (
-    <ul className={ hiddenMenu }>
-       <li>Productos</li> 
-       <li>Cotización</li> 
-       <li>Blog</li> 
-       <li>Contacto</li> 
-    </ul>
+    <div className="accordion-item">
+      <h2 className="accordion-header">
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <SlArrowDown />
+        </button>
+      </h2>
+      <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+          <Link to="/productos">
+            <li className="header_li_1">Productos</li>
+          </Link>
+          <Link to="/productos">
+            <li className="header_li_1">Cotización</li>
+          </Link>
+          <Link to="/productos">
+            <li className="header_li_1">Blog</li>
+          </Link>
+          <Link to="/productos">
+            <li className="header_li_1">Contacto</li>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
